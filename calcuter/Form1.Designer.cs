929b40b60be9881a37448e4117fad454c714f058
ACER <ACER@DESKTOP-UJCPPT5>
2025-03-06 04:19:36 -0800

    Add project files.

---
+++ calcuter/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace calcuter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.texNumber1 = new System.Windows.Forms.NumericUpDown();
+            this.texNumber2 = new System.Windows.Forms.NumericUpDown();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinese = new System.Windows.Forms.Button();
+            this.btnMultiple = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.texNumber1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.texNumber2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(389, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "اواین عدد:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(389, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "دومین عدد:";
+            // 
+            // texNumber1
+            // 
+            this.texNumber1.Location = new System.Drawing.Point(64, 9);
+            this.texNumber1.Name = "texNumber1";
+            this.texNumber1.Size = new System.Drawing.Size(286, 23);
+            this.texNumber1.TabIndex = 2;
+            // 
+            // texNumber2
+            // 
+            this.texNumber2.Location = new System.Drawing.Point(64, 39);
+            this.texNumber2.Name = "texNumber2";
+            this.texNumber2.Size = new System.Drawing.Size(285, 23);
+            this.texNumber2.TabIndex = 3;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnPlus.Location = new System.Drawing.Point(64, 99);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(75, 23);
+            this.btnPlus.TabIndex = 4;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = false;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMinese
+            // 
+            this.btnMinese.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnMinese.Location = new System.Drawing.Point(156, 99);
+            this.btnMinese.Name = "btnMinese";
+            this.btnMinese.Size = new System.Drawing.Size(75, 23);
+            this.btnMinese.TabIndex = 5;
+            this.btnMinese.Text = "-";
+            this.btnMinese.UseVisualStyleBackColor = false;
+            this.btnMinese.Click += new System.EventHandler(this.btnMinese_Click);
+            // 
+            // btnMultiple
+            // 
+            this.btnMultiple.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnMultiple.Location = new System.Drawing.Point(253, 99);
+            this.btnMultiple.Name = "btnMultiple";
+            this.btnMultiple.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiple.TabIndex = 6;
+            this.btnMultiple.Text = "*";
+            this.btnMultiple.UseVisualStyleBackColor = false;
+            this.btnMultiple.Click += new System.EventHandler(this.btnMultiple_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDivide.Location = new System.Drawing.Point(350, 99);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(75, 23);
+            this.btnDivide.TabIndex = 7;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = false;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 174);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiple);
+            this.Controls.Add(this.btnMinese);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.texNumber2);
+            this.Controls.Add(this.texNumber1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ماشین حساب ساده";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.texNumber1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.texNumber2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown texNumber1;
+        private System.Windows.Forms.NumericUpDown texNumber2;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinese;
+        private System.Windows.Forms.Button btnMultiple;
+        private System.Windows.Forms.Button btnDivide;
+    }
+}
+
---
+++ calcuter/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calcuter
+{
+    public partial class Form1: Form
+    {
+        ICalculate calculate;
+        public Form1()
+        {
+            InitializeComponent();
+            calculate = new Calculate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            if (ValidateInputs())
+            {
+                int sum = calculate.Plus((int)texNumber1.Value, (int)texNumber2.Value);
+                MessageBox.Show("sum is  " + sum);
+            }
+
+        }
+        bool ValidateInputs()
+        {
+            bool isValid = true;
+            if (texNumber1.Value == 0)
+            {
+
+                isValid = false;
+                MessageBox.Show("لطفا عدد اول وارد کن...");
+
+            }
+            else
+            {
+                if (texNumber2.Value == 0)
+                {
+                    isValid = false;
+                    MessageBox.Show("لطفا عدد دوم را وارد کن...");
+
+                }
+            }
+            return isValid;
+
+        }
+
+        private void btnMinese_Click(object sender, EventArgs e)
+        {
+            if (ValidateInputs())
+            {
+                int Minuse = calculate.Minus((int)texNumber1.Value, (int)texNumber2.Value);
+                MessageBox.Show("Minuse is  " + Minuse);
+            }
+        }
+
+        private void btnMultiple_Click(object sender, EventArgs e)
+        {
+            if (ValidateInputs())
+            {
+                int Multiple = calculate.Multiple((int)texNumber1.Value, (int)texNumber2.Value);
+                MessageBox.Show("Multiple is  " + Multiple);
+            }
+
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            if (ValidateInputs())
+            {
+                int Divide = calculate.Divide((int)texNumber1.Value, (int)texNumber2.Value);
+                MessageBox.Show("Divide is  " + Divide);
+            }
+        }
+    }
+}
